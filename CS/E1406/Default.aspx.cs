15f009a64bbd5eb15e11e9ad614a111dad189f1f
Alexey Khludov <alexey.khludov@devexpress.com>
2018-08-20 08:51:16 +0300

    fix .sln error, fix code

---
+++ CS/E1406/Default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.Web;
+using DevExpress.Web.ASPxGridView;
+using System;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void ASPxGridView1_CustomCallback(object sender, ASPxGridViewCustomCallbackEventArgs e) {
+        int colIndex = Convert.ToInt32(e.Parameters);
+        var grid = sender as ASPxGridView;
+
+        grid.Columns[colIndex].Visible = !grid.Columns[colIndex].Visible;
+
+        for (int i = 0; i < grid.Columns.Count; i++) {
+            if (grid.Columns[i].Visible)
+                grid.Columns[i].VisibleIndex = i;
+        }
+    }
+}
